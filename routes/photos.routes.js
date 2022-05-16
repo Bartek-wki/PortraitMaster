1be3394a34ac1562699704dfeb0f4a8c76e5ef62
Bartosz Wilki <b.wilki0109@gmail.com>
2022-05-16 14:06:04 +0200

    31.5

--- routes/photos.routes.js
+++ routes/photos.routes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const requestIp = require('request-ip');
 
 const photos = require('../controllers/photos.controller');
 
+const ipMiddleware = function(req, res, next) {
+  req.clientIp = requestIp.getClientIp(req);
+  next();
+};
+
 router.get('/photos', photos.loadAll);
 router.post('/photos', photos.add);
-router.put('/photos/vote/:id', photos.vote);
+router.put('/photos/vote/:id', ipMiddleware, photos.vote);
 
 module.exports = router;
